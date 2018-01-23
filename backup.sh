c2a11e7286a86bc32e08af7bfc6d3c89e48e65b2
CHN-STUDENT <CHN-STUDENT@outlook.com>
2018-01-23 15:42:36 +0800

    fixed bug

--- backup.sh
+++ backup.sh
@@ -102,51 +102,6 @@ function cfg_parser {
    return $EVAL_STATUS
 }
 
-   SECTION=$1
-   OLDIFS="$IFS"
-   IFS=' '$'\n'
-   if [ -z "$SECTION" ] 
-   then
-      fun="$(declare -F)"
-   else
-      fun="$(declare -F $PREFIX$SECTION)"
-      if [ -z "$fun" ]
-      then
-         echo "section $SECTION not found" >2
-         exit 1
-      fi
-   fi
-   fun="${fun//declare -f/}"
-   for f in $fun; do
-      [ "${f#$PREFIX}" == "${f}" ] && continue
-      item="$(declare -f ${f})"
-      item="${item##*\{}" # remove function definition
-      item="${item##*FUNCNAME*$PREFIX\};}" # remove clear section
-      item="${item/\}}"  # remove function close
-      item="${item%)*}" # remove everything after parenthesis
-      item="${item});" # add close parenthesis
-      vars=""
-      while [ "$item" != "" ]
-      do
-         newvar="${item%%=*}" # get item name
-         vars="$vars $newvar" # add name to collection
-         item="${item#*;}" # remove readed line
-      done
-      eval $f
-      echo "[${f#$PREFIX}]" # output section
-      for var in $vars; do
-         eval 'local length=${#'$var'[*]}' # test if var is an array
-         if [ $length == 1 ]
-         then
-            echo $var=\"${!var}\" #output var
-         else 
-            echo ";$var is an array" # add comment denoting var is an array
-            eval 'echo $var=\"${'$var'[*]}\"' # output array var
-         fi
-      done
-   done
-   IFS="$OLDIFS"
-}
 function cfg_unset {
    SECTION=$1
    OLDIFS="$IFS"
@@ -208,28 +163,6 @@ function cfg_clear {
    IFS="$OLDIFS"
 }
 
-   SECTION=$1
-   VAR=$2
-   OLDIFS="$IFS"
-   IFS=' '$'\n'
-   fun="$(declare -F $PREFIX$SECTION)"
-   if [ -z "$fun" ]
-   then
-      echo "section $SECTION not found" >2
-      exit 1
-   fi
-   fun="${fun//declare -f/}"
-   item="$(declare -f ${fun})"
-   #item="${item##* $VAR=*}" # remove var declaration
-   item="${item/\}}"  # remove function close
-   item="${item}
-    $VAR=(${!VAR})
-   "
-   item="${item}
-   }" # close function again
-
-   eval "function $item"
-}
 #Test harness
 if [ $# != 0 ]
 then
